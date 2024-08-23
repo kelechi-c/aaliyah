1b0a7d19e834941c8061285fd54a28801d93663f
Tensor <narutokelechi@gmail.com>
2024-08-23 11:39:24 +0100

    [add] trainer function, optimizer and mixed precision, trainer configs

---
+++ mobilenet/trainer.py
@@ -0,0 +1,88 @@
+import torch
+import wandb
+import os
+import gc
+from torch import optim
+from torch import nn
+from torch.cuda.amp import GradScaler
+from tqdm.auto import tqdm
+from safetensors.torch import save_model
+from .utils_config import config, count_params
+from .model import mobilenet
+from .dataload import train_loader
+
+criterion = nn.CrossEntropyLoss()  # loss function
+optimizer = optim.AdamW(params=mobilenet.parameters(), lr=config.lr)
+scaler = GradScaler()
+epochs = config.epoch_count
+
+
+param_count = count_params(mobilenet)
+print(param_count)
+
+
+# initilaize wandb
+wandb.login()
+train_run = wandb.init(project="HAR", name="har_mobilenet_1")
+wandb.watch(mobilenet)
+
+torch.cuda.empty_cache()
+gc.collect()
+
+
+def training_loop(
+    model=mobilenet, train_loader=train_loader, epochs=epochs, config=config
+):
+    model.train()
+    train_loss = 0.0
+
+    for epoch in tqdm(range(epochs + 1)):
+        optimizer.zero_grad()
+
+        torch.cuda.empty_cache()
+        print(f"Training epoch {epoch}")
+
+        for x, (image, label) in tqdm(enumerate(train_loader)):
+            image = image.to(config.device)
+            label = label.to(config.device)
+
+            # every iterations
+            torch.cuda.empty_cache()
+            gc.collect()
+
+            # Mixed precision training
+            with torch.autocast(device_type=config.dtype, dtype=config.dtype):
+                output = model(image)
+                train_loss = criterion(output, label.long())
+                train_loss = train_loss / config.grad_acc_step  # Normalize the loss
+
+            # Scales loss. Calls backward() on scaled loss to create scaled gradients.
+            scaler.scale(train_loss).backward()
+
+            if (x + 1) % config.grad_acc_step == 0:
+                # Unscales the gradients of optimizer's assigned params in-place
+
+                scaler.step(optimizer)
+                # Updates the scale for next iteration
+                scaler.update()
+                optimizer.zero_grad()
+            if (x + 1) % 5 == 0:
+                wandb.log({"loss": train_loss})
+
+        print(f"Epoch {epoch} of {epochs}, train_loss: {train_loss.item():.4f}")
+
+        print(f"Epoch @ {epoch} complete!")
+
+    print(
+        f"End metrics for run of {epochs}, train_loss: {train_loss.item():.4f}")
+
+    save_model(model, config.safetensor_file)
+
+
+training_loop()
+torch.cuda.empty_cache()
+gc.collect()
+
+
+print("mobilenet for Human Action Recognition pretraining complete")
+# Ciao
--- mobilenet/utils_config.py
+++ mobilenet/utils_config.py
@@ -3,11 +3,21 @@ import torch
 
 class config:
     lr = 1e-4
+    epoch_count = 50
     image_size = 224
+    grad_acc_step = 5
     batch_size = 32
+    dtype = torch.float16
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    safetensor_file = "har_mobilenet.safetensors"
     leaf_dataset_id = "yusuf802/plant-images"
     har_data_id = "Bingsu/Human_Action_Recognition"
-    dtype = torch.float16
     har_model_id = "tensorkelechi/HAR_mobilenet"
     plant_model_id = "tensorkelechi/leaf_disease_mobilenet"
+
+
+# Model parameter count
+def count_params(model: torch.nn.Module):
+    p_count = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    return p_count
